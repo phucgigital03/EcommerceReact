bd13b81f3537f70a34c4f7b8b8181c98abd0d892
phucnguyen <phucnguyendigital2003@gmail.com>
2025-02-27 14:59:59 +0700

    Display products on Home page

--- src/components/homes/Home.jsx
+++ src/components/homes/Home.jsx
@@ -1,14 +1,54 @@
-
-
-import HeroBanner from './HeroBanner'
+import { useDispatch, useSelector } from "react-redux";
+import HeroBanner from "./HeroBanner";
+import ProductCard from "../shared/ProductCard";
+import { useEffect } from "react";
+import { fetchProducts } from "../../store/actions";
+import Loader from "../shared/Loader";
+import { FaExclamationTriangle } from "react-icons/fa";
 
 function Home() {
+  const { products } = useSelector((state) => state.products);
+  const { isLoading, errorMessage } = useSelector((state) => state.errors);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, [dispatch]);
+
   return (
-    <div>
-        <HeroBanner/>
+    <div className="lg:px-14 sm:px-8 px-4">
+      <div className="py-6">
+        <HeroBanner />
+      </div>
+
+      <div className="py-5">
+        <div className="flex flex-col justify-center items-center space-y-2">
+          <h1 className="text-4xl text-slate-800 font-bold">Products</h1>
+          <span className="text-slate-800">
+            Discover our handpicked selection of top-rated items just for you!
+          </span>
+        </div>
+        {isLoading ? (
+          <Loader />
+        ) : errorMessage ? (
+          <div className="flex items-center justify-center h-[200px]">
+            <FaExclamationTriangle className="text-slate-800 text-3xl mr-2" />
+            <span className="text-slate-800 text-lg font-medium">
+              {errorMessage}
+            </span>
+          </div>
+        ) : (
+          <div className="pb-6 pt-14 grid 2xl:grid-cols-4  lg:grid-cols-3 sm:grid-cols-2 gap-y-6 gap-x-6">
+            {products &&
+              products
+                .slice(0, 4)
+                .map((item, i) => <ProductCard key={i} {...item} />)}
+          </div>
+        )}
+      </div>
+
     </div>
-  )
+  );
 }
 
-export default Home
-
+export default Home;
