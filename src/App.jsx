77c77a04ee1f6edf84baf81d84a76ec3d28d7ffb
phucnguyen <phucnguyendigital2003@gmail.com>
2025-04-14 11:51:32 +0700

    edit Contact page

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Home from "./components/homes/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./components/shared/NavBar";
 import About from "./components/about/About";
-import Contact from "./components/Contact";
+import Contact from "./components/contact/Contact";
 import { Toaster } from "react-hot-toast";
 import Cart from "./components/cart/Cart";
 import LogIn from "./components/auth/LogIn";
